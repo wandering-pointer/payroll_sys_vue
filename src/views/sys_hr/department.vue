e3d13908cfcc84fca83c57cd583b3c2d460365e6
Movingpoint-P <1632786437@qq.com>
2025-03-13 20:54:12 +0800

    完善Department搜索和新增功能

--- src/views/sys_hr/department.vue
+++ src/views/sys_hr/department.vue
@@ -13,7 +13,7 @@
                    :changePage="changePage"
       :page-size="page.size">
 				<template #toolbarBtn>
-					<el-button type="warning" :icon="CirclePlusFilled" @click="visible = true">新增</el-button>
+					<el-button type="warning" :icon="CirclePlusFilled" @click="AddVisible = true">新增</el-button>
 				</template>
 				<template #usable="{ rows }">
 					<el-tag :type="rows.usable ? 'success' : 'danger'">
@@ -23,11 +23,16 @@
 			</TableCustom>
 
 		</div>
-		<el-dialog :title="isEdit ? '编辑' : '新增'" v-model="visible" width="700px" destroy-on-close
-			:close-on-click-modal="false" @close="closeDialog">
-			<TableEdit :form-data="rowData" :options="options" :edit="isEdit" :update="updateData">
+		<el-dialog :title="'编辑'" v-model="EditVisible" width="700px" destroy-on-close
+               :close-on-click-modal="false" @close="closeEditDialog">
+			<TableEdit :form-data="editRowData" :options="options" :edit=true :update="editData">
 			</TableEdit>
 		</el-dialog>
+    <el-dialog :title="'新增'" v-model="AddVisible" width="700px" destroy-on-close
+               :close-on-click-modal="false" @close="closeAddDialog">
+      <TableEdit :form-data="addRowData" :options="options" :edit=true :update="insertData">
+      </TableEdit>
+    </el-dialog>
 		<el-dialog title="查看详情" v-model="visible1" width="700px" destroy-on-close>
 			<TableDetail :data="viewData">
         <template #usable="{ rows }">
@@ -47,20 +52,31 @@ import { CirclePlusFilled } from '@element-plus/icons-vue';
 import TableCustom from '@/components/table-custom.vue';
 import TableDetail from '@/components/table-detail.vue';
 import TableSearch from '@/components/table-search.vue';
-import { TableItem } from '@/types/table';
 import { FormOption, FormOptionList } from '@/types/form-option';
 import TableEdit from "@/components/table-edit.vue";
-import {listDepartment} from "@/api/forDepartment";
+import {deleteDepartment, insertDepartment, listDepartment, updateDepartment} from "@/api/forDepartment";
+import {Department} from "@/types/Department";
 
 // 查询相关
 const query = reactive({
 	name: '',
 });
 const searchOpt = ref<FormOptionList[]>([
-	{ type: 'input', label: '用户名：', prop: 'name' }
+  { type: 'input', label: '部门名称：', prop: 'name' },
+  { type: 'input', label: '部门编号：', prop: 'id' },
 ])
-const handleSearch = () => {
-	changePage(1);
+const handleSearch = async () => {
+  const res = await listDepartment({
+    limit: page.size,
+    offset: (page.index - 1) * page.size,
+    department: query,
+  })
+  if(res.data.code != 0){
+    ElMessage.error(res.data.message)
+  }
+  tableData.value = res.data.list;
+  page.total = res.data.total
+  page.index = 1;
 };
 
 // 表格相关
@@ -72,10 +88,10 @@ let columns = ref([
 ])
 const page = reactive({
 	index: 1,
-	size: 5,
+	size: 10,
 	total: 0,
 })
-const tableData = ref<TableItem[]>([]);
+const tableData = ref<Department[]>([]);
 const getData = async () => {
 	const res = await listDepartment({
     limit: page.size,
@@ -99,28 +115,50 @@ let options = ref<FormOption>({
 	list: [
 		{ type: 'input', label: '部门编号', prop: 'id', disabled: true, placeholder: '系统自动分配'},
 		{ type: 'input', label: '部门名称', prop: 'name', required: true },
-		{ type: 'switch', activeText: '正常', inactiveText: '禁用', label: '状态', prop: 'usable', required: true },
+		{ type: 'switch', activeText: '正常', inactiveText: '禁用', label: '状态', prop: 'usable'},
 	]
 })
-const visible = ref(false);
-const isEdit = ref(false);
-const rowData = ref({});
-const handleEdit = (row: TableItem) => {
-	rowData.value = { ...row };
-	isEdit.value = true;
-	visible.value = true;
+const EditVisible = ref(false);
+const AddVisible = ref(false);
+const editRowData = ref({});
+const addRowData = {
+  usable: true,
+  name: ''
+}
+const handleEdit = (row: Department) => {
+	editRowData.value = { ...row };
+	EditVisible.value = true;
 };
-const updateData = async () => {
-	closeDialog();
-
-
-
+const editData = async (form: Department) => {
+	closeEditDialog()
+  let res = await updateDepartment({department: form})
+    if (res.data.code == 0){
+      ElMessage.success(res.data.message)
+    }
+    else{
+      ElMessage.error(res.data.message)
+    }
 	getData();
 };
 
-const closeDialog = () => {
-	visible.value = false;
-	isEdit.value = false;
+const insertData = async (form: Department) => {
+  closeAddDialog()
+  let res = await insertDepartment({department: form})
+  if (res.data.code == 0){
+    ElMessage.success(res.data.message)
+  }
+  else{
+    ElMessage.error(res.data.message)
+  }
+  getData();
+};
+
+const closeEditDialog = () => {
+	EditVisible.value = false;
+};
+
+const closeAddDialog = () => {
+  AddVisible.value = false;
 };
 
 // 查看详情弹窗相关
@@ -129,7 +167,7 @@ const viewData = ref({
 	row: {},
 	list: []
 });
-const handleView = (row: TableItem) => {
+const handleView = (row: Department) => {
 	viewData.value.row = { ...row }
 	viewData.value.list = [
     { prop: 'id', label: '部门编号' },
@@ -140,8 +178,17 @@ const handleView = (row: TableItem) => {
 };
 
 // 删除相关
-const handleDelete = (row: TableItem) => {
-	ElMessage.success('删除成功');
+const handleDelete = async (row: Department) => {
+  const res = await deleteDepartment({department: row})
+  console.log(res)
+  if (res.data.code == 0) {
+    ElMessage.success(res.data.message)
+  }
+  else{
+    ElMessage.error(res.data.message)
+  }
+  page.index = 1
+  getData()
 }
 </script>
 
