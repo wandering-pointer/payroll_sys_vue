67b7646d90604fc53a935ae8ff72df236ada2979
Movingpoint-P <1632786437@qq.com>
2025-04-24 15:24:15 +0800

    考勤模拟打卡器功能

--- src/views/sys_debug/developerOptions.vue
+++ src/views/sys_debug/developerOptions.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
 import {ref} from "vue";
+import {insertAttendance} from "@/api/forAttendance";
 
 const empId = ref('')
 const date = ref('')
 const time = ref('')
+async function insert(type){
+  await insertAttendance({
+    empId: empId.value,
+    time: `${date.value}T${time.value}`,
+    type: type,
+  })
+}
 </script>
 
 <template>
 <div style="padding: 10px">
-  <div style="padding: 20px; width: 400px; background: #DDDDDD">
+  <div style="padding: 20px; width: 400px; background: #BEF">
     <div style="margin: 5px">
-      <label>考勤打卡模拟器</label>
+      <label>考勤打卡模拟终端</label>
     </div>
     <div style="margin-bottom: 5px">
       <el-text size="large">工号：</el-text>
@@ -30,7 +38,8 @@ const time = ref('')
       <el-text>{{date}}T{{ time }}</el-text>
     </div>
     <div style="margin-bottom: 5px; text-align: right">
-      <el-button>打卡</el-button>
+      <el-button @click="insert('in')">上班打卡</el-button>
+      <el-button @click="insert('out')">下班打卡</el-button>
     </div>
   </div>
 </div>
